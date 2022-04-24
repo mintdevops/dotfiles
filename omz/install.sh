a87e698f5fdac4174ccde62963a972c2dd4dba47
deyceg <deyna@mintsrc.co.uk>
2022-04-24 19:58:44 +0100

    initial commit

---
+++ omz/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+set -e
+
+if [ -d "${HOME}/.oh-my-zsh" ]; then
+  printf "oh-my-zsh is already installed\n"
+else
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
